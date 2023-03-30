59390a3fe1af9f6c5f53822f3a9ff008b757f8e7
Xiao Zhang <xxxiao971006@gmail.com>
2023-03-30 15:22:17 -0700

    4th commit

--- databaseConnection.js
+++ databaseConnection.js
@@ -2,7 +2,7 @@ const database = require("mongoose");
 const is_qoddi = process.env.IS_QODDI || false;
 const databaseName = "lab_example"
 const qoddiURI =
-	"mongodb+srv://xiao:Zxx53625140%40@cluster0.4ulcc.mongodb.net/lab_example?retryWrites=true&w=majority"
+	"mongodb+srv://xiao:Zxx53625140%40@cluster0.h5qplzn.mongodb.net/lab_example?retryWrites=true&w=majority"
 const localURI =
 	"mongodb://127.0.0.1:27017/lab_example?retryWrites=true&serverSelectionTimeoutMS=5000&connectTimeoutMS=10000&3t.uriVersion=3&3t.connection.name=localhost&3t.alwaysShowAuthDB=true&3t.alwaysShowDBFromUserRole=true"
 if (is_qoddi) {
