4c1d92bbc3f6234ee11ef2f3472075873bbc4709
Xiao Zhang <xxxiao971006@gmail.com>
2023-03-30 15:01:08 -0700

    1st commit

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,18 +1,17 @@
-const MongoClient = require("mongodb").MongoClient;
-
-const is_heroku = process.env.IS_HEROKU || false;
-
-
-const herokuURI = "mongodb://localhost/?authSource=admin&retryWrites=true&w=majority;"
-
-const localURI = "mongodb://localhost/?authSource=admin&retryWrites=true&w=majority;"
-
-if (is_heroku) {
-	var database = new MongoClient(herokuURI, {useNewUrlParser: true, useUnifiedTopology: true});
+const database = require("mongoose");
+const is_qoddi = process.env.IS_QODDI || false;
+const databaseName = "lab_example"
+const qoddiURI =
+	"mongodb+srv://xiao:Zxx53625140%40@cluster0.4ulcc.mongodb.net/lab_example?retryWrites=true&w=majority"
+const localURI =
+	"mongodb://127.0.0.1:27017/lab_example?retryWrites=true&serverSelectionTimeoutMS=5000&connectTimeoutMS=10000&3t.uriVersion=3&3t.connection.name=localhost&3t.alwaysShowAuthDB=true&3t.alwaysShowDBFromUserRole=true"
+if (is_qoddi) {
+	database.connect(qoddiURI, { useNewUrlParser: true, useUnifiedTopology: true });
 }
 else {
-	var database = new MongoClient(localURI, {useNewUrlParser: true, useUnifiedTopology: true});
+	database.connect(localURI, { useNewUrlParser: true, useUnifiedTopology: true });
 }
+
 
 module.exports = database;
 		
